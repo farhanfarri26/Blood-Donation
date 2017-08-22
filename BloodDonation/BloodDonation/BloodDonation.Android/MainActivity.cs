e487eef028da50c2918618d83bfb5476fc87b179
FARHAN\Farhan Farri <farhanfarri26@gmail.com>
2017-08-22 06:33:02 -0700

    Blood Donation App Project

---
+++ BloodDonation/BloodDonation/BloodDonation.Android/MainActivity.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace BloodDonation.Droid
+{
+    [Activity(Label = "Blood Donation", Icon = "@drawable/BloodBankLogo", Theme = "@style/splashscreen", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.Window.RequestFeature(WindowFeatures.ActionBar);
+            base.SetTheme(Resource.Style.MainTheme);
+
+            base.OnCreate(bundle);
+
+            global::Xamarin.Forms.Forms.Init(this, bundle);
+            LoadApplication(new App());
+        }
+    }
+}
+
---
+++ BloodDonation/BloodDonation/BloodDonation/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace BloodDonation
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            NavigationPage.SetHasNavigationBar(this, false);
+        }
+
+
+
+        private void BtnLogin_OnClicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Tabbed());
+        }
+
+        private void BtnSignup_OnClicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new SignupPage());
+        }
+    }
+}
